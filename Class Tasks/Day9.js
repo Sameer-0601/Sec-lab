08345c200b29c58585592118901ddbb650455f3a
JeevithaMahendran <24bcs121@kgcas.com>
2025-09-03 09:27:08 +0530

    Updated

---
+++ Class Tasks/Day9.js
@@ -0,0 +1,9 @@
+// Import and export modules
+
+// Definition:
+// Export allows you to make variables, functions, or
+// classes available to other File.
+
+// Import allows you to bring those into another File.
+
+// Used in modular Javascript (ES6+)
